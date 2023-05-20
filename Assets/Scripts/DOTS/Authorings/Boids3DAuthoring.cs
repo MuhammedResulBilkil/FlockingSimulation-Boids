f38fe394315da46df9b500f4f3a934cd4030ce2e
Resul <garnnet_221@hotmail.com>
2023-05-20 12:02:30 +0300

    ECS first steps and project version upgraded

---
+++ Assets/Scripts/DOTS/Authorings/Boids3DAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Boids3DAuthoring : MonoBehaviour
+{
+    
+}
+
+public class Boids3DBaker : Baker<Boids3DAuthoring>
+{
+    public override void Bake(Boids3DAuthoring authoring)
+    {
+        Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+        AddComponent(entity, new Boids3DComponent
+        {
+            velocity = float3.zero,
+            acceleration = float3.zero
+        });
+    }
+}
---
+++ Assets/Scripts/DOTS/Components/Boids3DComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct Boids3DComponent : IComponentData
+{
+    public float3 velocity;
+    public float3 acceleration;
+}
