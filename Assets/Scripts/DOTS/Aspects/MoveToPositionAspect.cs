ea64f4009b934d22182fbc91cc76299c7d30565f
Resul <garnnet_221@hotmail.com>
2023-05-20 15:33:57 +0300

    Need to get a component from an entity!

--- Assets/Scripts/DOTS/Aspects/MoveToPositionAspect.cs
+++ Assets/Scripts/DOTS/Aspects/MoveToPositionAspect.cs
@@ -27,6 +27,50 @@ public readonly partial struct MoveToPositionAspect : IAspect
             _localTransform.ValueRW.Position = new float3(_localTransform.ValueRO.Position.x, _localTransform.ValueRO.Position.y, boidsBoxSizeComponent.boxSize.z);
     }
     
+    public void Flock(QueryEnumerable<LocalTransform> queryEnumerableLocalTransforms, QueryEnumerable<Boids3DComponent> queryEnumerableBoids3DComponents)
+    {
+        float3 alignment = Align(queryEnumerableLocalTransforms, queryEnumerableBoids3DComponents);
+        //Vector3 cohesion = Cohesion();
+        //Vector3 separation = Separation();
+        
+        alignment *= 0.1f;
+        //cohesion *= GetPowerCohesion();
+        //separation *= GetPowerSeparation();
+        
+        _boids3DComponent.ValueRW.acceleration += alignment;
+        //_acceleration += cohesion;
+        //_acceleration += separation;
+    }
+    
+    private float3 Align(QueryEnumerable<LocalTransform> queryEnumerableLocalTransforms, QueryEnumerable<Boids3DComponent> queryEnumerableBoids3DComponents)
+    {
+        float3 steering = float3.zero;
+        int total = 0;
+
+        foreach (var localTransform in queryEnumerableLocalTransforms)
+        {
+            float distance = math.distance(_localTransform.ValueRO.Position, localTransform.Position);
+            
+            if (distance < 0.5f)
+            {
+                // TODO: Find a way to get the velocity of the other boid Entity!!!!!
+                //steering += localTransform.GetVelocity();
+                total++;
+            }
+        }
+        
+        if (total > 0)
+        {
+            steering /= total;
+            steering = math.normalize(steering) * 0.16f;
+            steering -= _boids3DComponent.ValueRO.velocity;
+            steering = math.clamp(steering, -0.32f, 0.32f);
+            //steering = Vector3.ClampMagnitude(steering, 0.32f);
+        }
+
+        return steering;
+    }
+    
     public void Move(float deltaTime, RefRW<RandomComponent> randomComponent, float3 boxSize)
     {
         //float3 direction = math.normalize(_targetPosition.ValueRO.targetPosition - _localTransform.ValueRO.Position);
@@ -56,4 +100,7 @@ public readonly partial struct MoveToPositionAspect : IAspect
             randomComponent.ValueRW.random.NextFloat(-boxSize.y, boxSize.y),
             randomComponent.ValueRW.random.NextFloat(-boxSize.z, boxSize.z));
     }
+    
+    public RefRW<LocalTransform> GetRefRWLocalTransform() => _localTransform;
+    public RefRW<Boids3DComponent> GetRefRWBoids3DComponent() => _boids3DComponent;
 }
--- Assets/Scripts/DOTS/Systems/ISystems/Boids3DISystem.cs
+++ Assets/Scripts/DOTS/Systems/ISystems/Boids3DISystem.cs
@@ -1,13 +1,19 @@
 ﻿using Unity.Burst;
+using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Transforms;
 
 [BurstCompile]
 public partial struct Boids3DISystem : ISystem
 {
     private RefRW<RandomComponent> _randomComponent;
     private BoidsBoxSizeComponent _boxSizeComponent;
+    private QueryEnumerable<LocalTransform> _queryEnumerableLocalTransforms;
+    private QueryEnumerable<Boids3DComponent> _queryEnumerableBoids3DComponents;
+    private NativeArray<Entity> _entityQuery;
+    
     private float _deltaTime;
 
     [BurstCompile]
@@ -22,6 +28,10 @@ public partial struct Boids3DISystem : ISystem
         _randomComponent = SystemAPI.GetSingletonRW<RandomComponent>();
         _boxSizeComponent = SystemAPI.GetSingleton<BoidsBoxSizeComponent>();
         
+        _queryEnumerableLocalTransforms = SystemAPI.Query<LocalTransform>();
+        _queryEnumerableBoids3DComponents = SystemAPI.Query<Boids3DComponent>();
+        _entityQuery = SystemAPI.QueryBuilder().WithAspect<MoveToPositionAspect>().Build().ToEntityArray(Allocator.TempJob);
+
         _deltaTime = SystemAPI.Time.DeltaTime;
         
         JobHandle calculateEdgeJobHandle = new CalculateEdgeJob
@@ -30,6 +40,16 @@ public partial struct Boids3DISystem : ISystem
         }.ScheduleParallel(state.Dependency);
         
         calculateEdgeJobHandle.Complete();
+        
+        JobHandle calculateFlockJobHandle = new CalculateFlockJob
+        {
+            queryEnumerableLocalTransforms = _queryEnumerableLocalTransforms,
+            queryEnumerableBoids3DComponents = _queryEnumerableBoids3DComponents
+        }.ScheduleParallel(state.Dependency);
+        
+        calculateFlockJobHandle.Complete();
+        
+        
     }
 
     [BurstCompile]
@@ -48,3 +68,16 @@ public partial struct CalculateEdgeJob : IJobEntity
         moveToPositionAspect.Edges(boidsBoxSizeComponent);
     }
 }
+
+[BurstCompile]
+public partial struct CalculateFlockJob : IJobEntity
+{
+    public QueryEnumerable<LocalTransform> queryEnumerableLocalTransforms;
+    public QueryEnumerable<Boids3DComponent> queryEnumerableBoids3DComponents;
+
+    public void Execute(MoveToPositionAspect moveToPositionAspect)
+    {
+        moveToPositionAspect.Flock(queryEnumerableLocalTransforms, queryEnumerableBoids3DComponents);
+    }
+}
+
